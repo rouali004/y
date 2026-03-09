bebf49d0cdae8b7f63a6997126f111435488dcca
rouali004 <thelordreally@gmail.com>
2026-03-09 16:47:25 +0300

    Update service worker to cache additional icons
    Added icons to cache and implemented service worker activation logic.

--- sw.js
+++ sw.js
@@ -2,9 +2,12 @@ const CACHE_NAME = 'ziyarat-ashura-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
+// Install service worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -12,9 +15,25 @@ self.addEventListener('install', event => {
   );
 });
 
+// Fetch from cache
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+  );
+});
+
+// Update service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
